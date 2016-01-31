f5a84e7a6c2d72acbbbada8ba5b3d2af9915e5a6
Shingo Kawamura <blp1526@gmail.com>
2016-01-31 13:49:59 +0900

    Add tray

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const app = electron.app;
+const Menu = electron.Menu;
+const Tray = electron.Tray;
+const spawn = require('child_process').spawn;
+
+const capture = function() {
+  spawn('say', ['foo'])
+};
+
+var appIcon = null;
+
+app.on('ready', function(){
+  appIcon = new Tray('./images/camera.png');
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Capture', click: function() { capture(); } },
+    { type: 'separator' },
+    { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); } }
+  ]);
+  appIcon.setToolTip('Cammy');
+  appIcon.setContextMenu(contextMenu);
+});
